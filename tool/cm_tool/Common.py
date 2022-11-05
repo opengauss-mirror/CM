282d5c71766d21334e896c55275eb790b57e8926
xue_meng_en <1836611252@qq.com>
2022-11-05 10:40:15 +0800

    卸载CM前增加互信校验

--- tool/cm_tool/Common.py
+++ tool/cm_tool/Common.py
@@ -60,3 +60,19 @@ def checkXMLFile(xmlFile):
         CMLog.exitWithError(ErrorCode.GAUSS_502["GAUSS_50210"] % "xmlFile")
     if not os.access(xmlFile, os.R_OK):
         CMLog.exitWithError(ErrorCode.GAUSS_501["GAUSS_50100"] % (xmlFile, "current user"))
+
+def checkHostsTrust(hosts, localhost = ""):
+    """
+    check trust between current host and the given hosts
+    """
+    hostsWithoutTrust = []
+    for host in hosts:
+        if host == localhost:
+            continue
+        checkTrustCmd = "ssh -o ConnectTimeout=3 -o ConnectionAttempts=5 -o PasswordAuthentication=no " \
+            "-o StrictHostKeyChecking=no %s 'pwd > /dev/null'" % host
+        status, output = subprocess.getstatusoutput(checkTrustCmd)
+        if status != 0:
+            hostsWithoutTrust.append(host)
+    if hostsWithoutTrust != []:
+        CMLog.exitWithError(ErrorCode.GAUSS_511["GAUSS_51100"] % ','.join(hostsWithoutTrust))
